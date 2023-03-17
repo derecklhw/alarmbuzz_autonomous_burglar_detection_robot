57fb7efac294e585f78609293a413d1abb4f5e3f
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2023-03-17 22:13:22 +0400

    feat: human detection and add model

--- humanDetection.py
+++ humanDetection.py
@@ -2,41 +2,52 @@ import cv2
 import numpy as np
 from keras.models import load_model
 
-model = load_model(r"human.h5")
 
-firelist=['Human','Not-Human']
-def process_frame(image):
-    # Preprocess the frame
-    resized = cv2.resize(image, (86, 48))
-    image_rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+class HumanDetection:
+    def __init__(self, frame, model):
+        self.frame = frame
+        self.model = model
+        self.humanlist = ["NOT-HUMAN", "HUMAN"]
 
-    # Use the model to predict whether there is fire in the frame
-    prediction = model.predict(np.expand_dims(image_rgb, axis=0))
-   
+    def process_frame(self):
+        # Preprocess the frame
+        resized = cv2.resize(frame, (86, 48))
+        image_rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
 
-    label = firelist[np.argmax(prediction)]
-    print (prediction)
+        # Use the model to predict whether there is fire in the frame
+        prediction = model.predict(np.expand_dims(image_rgb, axis=0))
 
-    cv2.putText(image, label, (50, 50),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        print(
+            f"Prediction is: {prediction} and the label is: {[np.argmax(prediction)]}"
+        )
+        label = self.humanlist[np.argmax(prediction)]
 
-    return image
+        cv2.putText(frame, label, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-
-# if __name__ == "__main__":
-#     # Start capturing the camera feed
-#     cap = cv2.VideoCapture(0)
-#     # Load the trained model
+        return frame, label
 
 
-while True:
+if __name__ == "__main__":
+    # Start capturing the camera feed
     cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    # Initialize the class
-    # img=cv2.imread(r"dereck.jpeg")
-    fire = process_frame(frame)
-    cv2.imshow("Fire Detection", fire)
+    # Load the trained model
+    model = load_model(r"Models/human.h5")
+
+    while True:
+        ret, frame = cap.read()
+
+        # Initialize the class
+        human = HumanDetection(frame, model)
+
+        # Process the frame
+        processed_frame, processed_label = human.process_frame()
+
+        # Display the frame
+        cv2.imshow("Human Detection", processed_frame)
+
+        if processed_label == "HUMAN":
+            print("Human Detected")
 
         # Press q to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
