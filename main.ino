2490c6dba2d79e434b1060b50d549dc03428bda5
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2023-03-27 22:17:19 +0400

    feat: working serial connection from pi to arduino which trigger the buzzer

--- main.ino
+++ main.ino
@@ -1,17 +1,24 @@
+const int buzzer = 9; // buzzer to arduino pin 9
+
 void setup() 
 {
     Serial.begin(9600);
     // wait for serial port to connect
+    Serial.println("start");
+    pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
     while (!Serial){
-   
-  }
+    ; //wait for serial port to connect
+    }
 }
 
 void loop() {
   if (Serial.available() > 0) {
     String inputString = Serial.readStringUntil('\n');
     if (inputString == "run") {
-   
+        tone(buzzer, 1000); // Send 1KHz sound signal...
+        delay(1000);        // ...for 1 sec
+        noTone(buzzer);     // Stop sound...
+        delay(1000);        // ...for 1sec
     }
   }
 }
--- main.py
+++ main.py
@@ -1,50 +1,9 @@
 import serial
-import cv2
-from keras.models import load_model
 import time
-
-from humanDetection import HumanDetection
-
-def triggerHumanDetection():
-    # Start capturing the camera feed
-    cap = cv2.VideoCapture(0)
-    # Load the trained model
-    model = load_model(r"Models/human.h5")
-
-    # Set the start time
-    start_time = time.time()
-
-    # Set the duration to 10 seconds
-    duration = 10
-
-    while (time.time() - start_time) < duration:
-        # Code to be executed during the 10 second limit
-        ret, src = cap.read()
-        # Use flip code 0 to flip vertically
-        frame = cv2.flip(src, 0)
-
-        # Initialize the class
-        human = HumanDetection(frame, model)
-
-        # Process the frame
-        processed_frame, processed_label = human.process_frame()
-
-        # Display the frame
-        cv2.imshow("Human Detection", processed_frame)
-
-        if processed_label == "HUMAN":
-            return "human detected"
-
-        # Press q to quit
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    return "no human detected"
 
 if __name__ == "__main__":
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.flush()
-
     time.sleep(2)
+
     while True:
         ser.write(b'run\n')
-        print("running")
