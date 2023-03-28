5edd6ee49a8a492ef6fd9af9bc9dce132b3c3d01
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2023-03-28 19:23:36 +0400

    feat: implement previous code in classes

--- humanDetection/humanDetectionHoGDescriptor.py
+++ humanDetection/humanDetectionHoGDescriptor.py
@@ -1,42 +1,36 @@
 import cv2
 import imutils
-   
-# Initializing the HOG person
-# detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-   
-cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-   
-while cap.isOpened():
-    # Reading the video stream
-    ret, src = cap.read()
-    image = cv2.flip(src, 0)
-    if ret:
-        image = imutils.resize(image, 
-                               width=min(400, image.shape[1]))
-   
-        # Detecting all the regions 
-        # in the Image that has a 
-        # pedestrians inside it
-        (regions, _) = hog.detectMultiScale(image,
-                                            winStride=(4, 4),
-                                            padding=(4, 4),
-                                            scale=1.05)
-   
-        # Drawing the regions in the 
-        # Image
-        for (x, y, w, h) in regions:
-            cv2.rectangle(image, (x, y),
-                          (x + w, y + h), 
-                          (0, 0, 255), 2)
-            print("Human detected")
-   
-        # Showing the output Image
-        cv2.imshow("Image", image)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-    else:
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+class PedestrianDetector:
+    def __init__(self, camera_id):
+        self.hog = cv2.HOGDescriptor()
+        self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        self.cap = cv2.VideoCapture(camera_id, cv2.CAP_V4L2)
+        
+    def detect_pedestrians(self):
+        while self.cap.isOpened():
+            ret, src = self.cap.read()
+            image = cv2.flip(src, 0)
+            if ret:
+                image = imutils.resize(image, 
+                                       width=min(400, image.shape[1]))
+                (regions, _) = self.hog.detectMultiScale(image,
+                                                          winStride=(4, 4),
+                                                          padding=(4, 4),
+                                                          scale=1.05)
+                for (x, y, w, h) in regions:
+                    cv2.rectangle(image, (x, y),
+                                  (x + w, y + h), 
+                                  (0, 0, 255), 2)
+                    return("Human detected")
+                cv2.imshow("Image", image)
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+detector = PedestrianDetector(0)  # camera_id = 0 for default camera
+print(detector.detect_pedestrians())
+
