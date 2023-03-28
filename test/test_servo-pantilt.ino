c1483e797a99139db3d72b2754fc2d6351a155bc
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2023-03-28 16:12:16 +0400

    feat: split servo pantilt movement in functions

--- test/test_servo-pantilt.ino
+++ test/test_servo-pantilt.ino
@@ -8,51 +8,66 @@ int posY = 0;  // initial position of the vertical servo
 
 void setup()
 {
-    servoX.attach(9);   // attach the horizontal servo to pin 9
-    servoY.attach(10);  // attach the vertical servo to pin 10
+    servoX.attach(3);   // attach the horizontal servo to pin 9
+    servoY.attach(2);  // attach the vertical servo to pin 10
 }
 
 void loop()
 {
-    for (posY = 0; posY <= 90; posY += 1)
+    servo_start();
+
+    servo_sweep();
+
+    servo_end();
+    delay(1000);
+}
+
+void servo_start()
+{
+    for (posY = posY; posY <= 45; posY += 1)
     {
         servoY.write(posY); // move the vertical servo
-        delay(15);          // delay between movements
+        delay(50);          // delay between movements
     }
 
-    for (posX = 90; posX >= 0; posX -= 1)
+    for (posX = posX; posX >= 0; posX -= 1)
     {
         servoX.write(posX); // move the horizontal servo
-        delay(15);          // delay between movements
+        delay(50);          // delay between movements
     }
+}
 
+void servo_sweep()
+{
     unsigned long start_time = millis(); // get the current time
     while (millis() - start_time < 10000)
     { // run the loop for 10 seconds
 
-        for (posX = 0; posX <= 180; posX += 1)
+        for (posX = posX; posX <= 180; posX += 1)
         {
             servoX.write(posX); // move the horizontal servo
-            delay(15);          // delay between movements
+            delay(50);          // delay between movements
         }
-        for (posX = 180; posX >= 0; posX -= 1)
+        for (posX = posX; posX >= 0; posX -= 1)
         {
             servoX.write(posX); // move the horizontal servo
-            delay(15);          // delay between movements
+            delay(50);          // delay between movements
         }
     }
+}
 
+void servo_end()
+{
     // stop the servos and wait for 1 second before starting the next loop
     for (posX = posX; posX <= 90; posX += 1)
     {
         servoX.write(posX); // move the horizontal servo
-        delay(15);          // delay between movements
+        delay(50);          // delay between movements
     }
 
-    for (posY = 90; posY >= 0; posY -= 1)
+    for (posY = posY; posY >= 0; posY -= 1)
     {
         servoY.write(posY); // move the vertical servo
-        delay(15);          // delay between movements
+        delay(50);          // delay between movements
     }
-    delay(1000);
 }
