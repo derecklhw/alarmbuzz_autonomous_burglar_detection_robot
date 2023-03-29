06051cf7f467ac24304082db2dda894d539aa601
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2023-03-29 23:05:51 +0400

    feat: add webhook

--- humanDetection/hogDescriptor.py
+++ humanDetection/hogDescriptor.py
@@ -2,6 +2,7 @@ import cv2
 import imutils
 import time
 from datetime import datetime
+from dhooks import Webhook, File
 
 class HumanDetector:
     def __init__(self, camera_id):
@@ -26,12 +27,26 @@ class HumanDetector:
                     cv2.rectangle(image, (x, y),
                                   (x + w, y + h), 
                                   (0, 0, 255), 2)
-                    # Take a photo when humans are detected
-                    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-                    cv2.imwrite(f"images/photo_{current_time}.jpg", image)
 
+                    # Take a photo when humans are detected
+                    current_time = datetime.now()
+                    current_time_image_format = current_time.strftime("%Y%m%d_%H%M%S")
+                    human_detection_image_path = f"images/photo_{current_time_image_format}.jpg"
+                    cv2.imwrite(human_detection_image_path, image)
+                        
                     self.cap.release()
                     cv2.destroyAllWindows()
+                    
+                    # Create a Webhook object and pass in the URL of the Discord webhook
+                    hook = Webhook("https://discord.com/api/webhooks/1090684363714351144/NMGFwquH-LvDjJBHL8SmxQtKRjFjT8ZQ6Nqb-r8IMZP7fkqLJHylMijY1J8FRv4zH3Us")
+
+                    dt_string = current_time.strftime("%d/%m/%Y %H:%M:%S")
+
+                    data = f"Attention! Our sensors have detected the presence of a human being as of {dt_string}.\nFor safety reasons, please stand still and wait for further instructions."
+                    human_detection_image = File(human_detection_image_path)
+                    
+                    hook.send(data, file=human_detection_image)
+                
                     return True
                 cv2.imshow("Image", image)
                 if cv2.waitKey(25) & 0xFF == ord('q'):
